6f8de4072e968f71715ab51366745eca8b5a6ab0
Lxc <45941187+xc912@users.noreply.github.com>
2021-04-25 23:32:05 +0800

    Add files via upload

--- js/aue.js
+++ js/aue.js
@@ -3,6 +3,7 @@ document.write("<script src='https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jque
 const aue = {};
 const aueMethod = {};
 const monitorTable = [];
+const modelTable = [];
 
 class Aue {
 	constructor(global) {
@@ -56,10 +57,16 @@ class Aue {
 						if (child.innerText.match(/{{(.+)}}/g) !== null) {
 							Aue.setInnerText(child, child.innerText, true);
 						}
-						if (child.hasAttribute('text')) {
-							Aue.setText(child, child.getAttribute("text"), true);
+						if (child.hasAttribute('a-value')) {
+							Aue.setValue(child, child.getAttribute("a-value"), true);
 						}
-						if (child.hasAttribute('click')) {
+						if (child.hasAttribute('a-model')) {
+							Aue.setModel(child, true);
+						}
+						if (child.hasAttribute('a-checked')) {
+							Aue.setChecked(child, child.getAttribute("a-checked"), true);
+						}
+						if (child.hasAttribute('a-click')) {
 							Aue.setClick(child);
 						}
 					}
@@ -78,13 +85,14 @@ class Aue {
 		newInnerText = newInnerText.replace(new RegExp('{{', "g"), '');
 		newInnerText = newInnerText.replace(new RegExp('}}', "g"), '');
 		child.innerText = newInnerText;
-		if (monitor)
+
+		if (monitor && !child.hasAttribute('a-once'))
 			Aue.addMonitor(child, 'innerText', {
 				'InitialValue': InitialValue
 			}, $.trim(variableName));
 	}
 
-	static setText(child, InitialValue, monitor) {
+	static setValue(child, InitialValue, monitor) {
 		const variableName = InitialValue.substring(InitialValue.indexOf("{{") + 2, InitialValue.lastIndexOf("}}"));
 		if (aue[$.trim(variableName)] === undefined) {
 			child.value = InitialValue;
@@ -95,14 +103,59 @@ class Aue {
 		newValue = newValue.replace(new RegExp('{{', "g"), '');
 		newValue = newValue.replace(new RegExp('}}', "g"), '');
 		child.value = newValue;
-		if (monitor)
-			Aue.addMonitor(child, 'text', {
+
+		if (monitor && !child.hasAttribute('a-once'))
+			Aue.addMonitor(child, 'value', {
 				'InitialValue': InitialValue
 			}, $.trim(variableName));
 	}
 
+	static setModel(child, model) {
+		const variableName = child.getAttribute("a-model");
+		if (aue[$.trim(variableName)] === undefined) {
+			return;
+		}
+		const variableValue = aue[$.trim(variableName)] === undefined ? variableName : aue[$.trim(variableName)];
+		let newValue = variableName.replace(new RegExp(variableName, "g"), variableValue);
+		switch (child.type) {
+			case 'checkbox':
+			case 'radio':
+				child.checked = newValue === 'true' ? true : false;
+				if (model) {
+					$(child).bind("change", function(event) {
+						aue[$.trim(variableName)] = $(this).is(':checked');
+					});
+				}
+				break;
+			default:
+				child.value = newValue;
+				if (model) {
+					$(child).bind("input propertychange", function(event) {
+						aue[$.trim(variableName)] = $(child).val();
+					});
+				}
+				break;
+		}
+
+		if (model)
+			Aue.addModel(child, 'model', $.trim(variableName));
+	}
+
+	static setChecked(child, InitialValue, monitor) {
+		if (aue[$.trim(InitialValue)] === undefined) {
+			return;
+		}
+		const variableValue = aue[$.trim(InitialValue)] === undefined ? InitialValue : aue[$.trim(InitialValue)];
+		child.checked = variableValue;
+
+		if (monitor && !child.hasAttribute('a-once'))
+			Aue.addMonitor(child, 'checked', {
+				'InitialValue': InitialValue
+			}, $.trim(InitialValue));
+	}
+
 	static setClick(child) {
-		const funName = child.getAttribute("click");
+		const funName = child.getAttribute("a-click");
 		child.addEventListener('click', function() {
 			try {
 				child.text = child.value;
@@ -113,7 +166,7 @@ class Aue {
 		});
 	}
 
-	static setMonitor(key, key2) {
+	static setMonitor(key) {
 		const v = key;
 		Object.defineProperty(aue, key, {
 			get: function() {
@@ -121,6 +174,15 @@ class Aue {
 			},
 			set: function(value) {
 				key = value;
+				for (const el of modelTable) {
+					let attribute = el['attribute'];
+					if (el['key'] === v)
+						switch (attribute) {
+							case 'model':
+								Aue.setModel(el['child'], false);
+								break;
+						}
+				}
 				for (const el of monitorTable) {
 					let attribute = el['attribute'];
 					let parameter = el['parameter'];
@@ -129,13 +191,24 @@ class Aue {
 							case 'innerText':
 								Aue.setInnerText(el['child'], parameter['InitialValue'], false);
 								break;
-							case 'text':
-								Aue.setText(el['child'], parameter['InitialValue'], false);
+							case 'value':
+								Aue.setValue(el['child'], parameter['InitialValue'], false);
+								break;
+							case 'checked':
+								Aue.setChecked(el['child'], parameter['InitialValue'], false);
 								break;
 						}
 				}
 			}
 		});
+	}
+
+	static addModel(child, attribute, key) {
+		let temp = {};
+		temp['key'] = key;
+		temp['child'] = child;
+		temp['attribute'] = attribute;
+		modelTable.push(temp);
 	}
 
 	static addMonitor(child, attribute, parameter, key) {
